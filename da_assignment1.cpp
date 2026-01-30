ed30c293bb3833591cf9ffee10e840e7cb555850
farry9891 <siddiquifarhan0102@gmail.com>
2026-01-30 10:23:35 +0530

    da_assignment1.cpp

--- da_assignment1.cpp
+++ da_assignment1.cpp
@@ -3,36 +3,30 @@
 using namespace std;
 
 int main() {
-    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int target = 23;   
-    int st = 0;
-    int en = n - 1;
-    int ans = -1;
+    int nums[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int len = sizeof(nums) / sizeof(nums[0]);
+    int key = 23;
 
-    while (st <= en) {
-        int mid = st + (en - st) / 2;
+    int left = 0, right = len - 1;
+    int index = -1;
 
-        if (arr[mid] == target) {
-            ans = mid;
+    while (left <= right) {
+        int center = left + (right - left) / 2;
+
+        if (nums[center] == key) {
+            index = center;
             break;
-}
-        
-        if (arr[mid] < target) {
-            st = mid + 1;
-        } 
-        else {
-            en = mid - 1;
-        }}
-
-    if (ans != -1) {
-        cout << ans << endl;
-    } else {
-        cout << -1 << endl;
+        } else if (nums[center] < key) {
+            left = center + 1;
+        } else {
+            right = center - 1;
+        }
     }
 
+    cout << index << endl;
     return 0;
 }
+
 //Output= 5
 
 //____________________________________________________________________________________________________//
@@ -41,52 +35,58 @@ int main() {
 
 #include <iostream>
 using namespace std;
-void merge(int arr[], int st, int mid, int en) {
-    int n1 = mid - st + 1;
-    int n2 = en - mid;
-    int L[n1], R[n2];
-    for (int i = 0; i < n1; i++) L[i] = arr[st + i];
-    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
-    int i = 0, j = 0, k = st;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;} 
-        else {
-            arr[k] = R[j];
-            j++;}
-        k++;}
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;}
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;}
+
+void combine(int data[], int low, int mid, int high) {
+    int sizeA = mid - low + 1;
+    int sizeB = high - mid;
+
+    int first[sizeA], second[sizeB];
+
+    for (int i = 0; i < sizeA; i++) first[i] = data[low + i];
+    for (int j = 0; j < sizeB; j++) second[j] = data[mid + 1 + j];
+
+    int i = 0, j = 0, k = low;
+
+    while (i < sizeA && j < sizeB) {
+        if (first[i] <= second[j]) {
+            data[k++] = first[i++];
+        } else {
+            data[k++] = second[j++];
+        }
+    }
+
+    while (i < sizeA) data[k++] = first[i++];
+    while (j < sizeB) data[k++] = second[j++];
 }
-void mergeSort(int arr[], int st, int en) {
-    if (st < en) {
-        int mid = st + (en - st) / 2;
-        mergeSort(arr, st, mid);
-        mergeSort(arr, mid + 1, en);
-        merge(arr, st, mid, en); }
+
+void mergeAlgo(int data[], int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
+        mergeAlgo(data, low, mid);
+        mergeAlgo(data, mid + 1, high);
+        combine(data, low, mid, high);
+    }
 }
-void display(int arr[], int size) {
-    for (int i = 0; i < size; i++) cout << arr[i] << " ";
-    cout << endl;}
+
+void show(int data[], int size) {
+    for (int i = 0; i < size; i++) cout << data[i] << " ";
+    cout << endl;
+}
+
 int main() {
-    int arr1[] = {12, 11, 13, 5, 6, 7};
-    int n1 = sizeof(arr1) / sizeof(arr1[0]);
-    mergeSort(arr1, 0, n1 - 1);
-    display(arr1, n1);
-    int arr2[] = {38, 27, 43, 3, 9, 82, 10};
-    int n2 = sizeof(arr2) / sizeof(arr2[0]);
-    mergeSort(arr2, 0, n2 - 1);
-    display(arr2, n2);
+    int list1[] = {12, 11, 13, 5, 6, 7};
+    int n1 = sizeof(list1) / sizeof(list1[0]);
+    mergeAlgo(list1, 0, n1 - 1);
+    show(list1, n1);
+
+    int list2[] = {38, 27, 43, 3, 9, 82, 10};
+    int n2 = sizeof(list2) / sizeof(list2[0]);
+    mergeAlgo(list2, 0, n2 - 1);
+    show(list2, n2);
 
     return 0;
 }
+
 
 //Output= 5 6 7 11 12 13, 3 9 10 27 38 43 82 
 
@@ -97,39 +97,47 @@ int main() {
 #include <iostream>
 using namespace std;
 
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;}
-
-int partition(int arr[], int st, int en) {
-    int pivot = arr[en];
-    int i = st - 1;
-
-    for (int j = st; j < en; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }}
-    swap(arr[i + 1], arr[en]);
-    return i + 1;
+void swapVals(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
 }
-void quickSort(int arr[], int st, int en) {
-    if (st < en) {
-        int pidx = partition(arr, st, en);
-        quickSort(arr, st, pidx - 1);
-        quickSort(arr, pidx + 1, en); }}
+
+int divide(int arr[], int low, int high) {
+    int pivotVal = arr[high];
+    int idx = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivotVal) {
+            idx++;
+            swapVals(arr[idx], arr[j]);
+        }
+    }
+
+    swapVals(arr[idx + 1], arr[high]);
+    return idx + 1;
+}
+
+void quickAlgo(int arr[], int low, int high) {
+    if (low < high) {
+        int p = divide(arr, low, high);
+        quickAlgo(arr, low, p - 1);
+        quickAlgo(arr, p + 1, high);
+    }
+}
 
 int main() {
-    int arr[] = {4, 2, 6, 9, 2};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, n - 1);
+    int values[] = {4, 2, 6, 9, 2};
+    int n = sizeof(values) / sizeof(values[0]);
 
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";}
+    quickAlgo(values, 0, n - 1);
+
+    for (int i = 0; i < n; i++) cout << values[i] << " ";
     cout << endl;
+
     return 0;
 }
+
 
 //Output= 2 2 4 6 9 
 
@@ -141,36 +149,44 @@ int main() {
 #include <algorithm>
 using namespace std;
 
-int crossSum(int arr[], int st, int mid, int en) {
-    int sum = 0;
-    int left_sum = INT_MIN;
-    for (int i = mid; i >= st; i--) {
-        sum += arr[i];
-        if (sum > left_sum) left_sum = sum;
+int midSum(int arr[], int low, int mid, int high) {
+    int total = 0;
+    int leftMax = INT_MIN;
+
+    for (int i = mid; i >= low; i--) {
+        total += arr[i];
+        leftMax = max(leftMax, total);
     }
 
-    sum = 0;
-    int right_sum = INT_MIN;
-    for (int i = mid + 1; i <= en; i++) {
-        sum += arr[i];
-        if (sum > right_sum) right_sum = sum;}
-    return left_sum + right_sum;
+    total = 0;
+    int rightMax = INT_MIN;
+
+    for (int i = mid + 1; i <= high; i++) {
+        total += arr[i];
+        rightMax = max(rightMax, total);
+    }
+
+    return leftMax + rightMax;
 }
 
-int maxSub(int arr[], int st, int en) {
-    if (st == en) return arr[st];
-    int mid = st + (en - st) / 2;
+int maxSegment(int arr[], int low, int high) {
+    if (low == high) return arr[low];
 
-    int left_max = maxSub(arr, st, mid);
-    int right_max = maxSub(arr, mid + 1, en);
-    int mid_max = crossSum(arr, st, mid, en);
-    return max({left_max, right_max, mid_max});
+    int mid = low + (high - low) / 2;
+
+    int leftAns = maxSegment(arr, low, mid);
+    int rightAns = maxSegment(arr, mid + 1, high);
+    int crossAns = midSum(arr, low, mid, high);
+
+    return max({leftAns, rightAns, crossAns});
 }
 
 int main() {
-    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << maxSub(arr, 0, n - 1) << endl;
+    int nums[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    cout << maxSegment(nums, 0, n - 1) << endl;
     return 0;
 }
+
 //Output= 7
